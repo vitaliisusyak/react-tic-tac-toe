8e56b4efb06b4bf403060c3a4c3c006bf1d574fc
vsusiak8 <vitalii.susiak@sensiaglobal.com>
2025-02-13 09:53:55 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Player from "./components/Player.jsx";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name='Player 1' symbol='X'></Player>
+          <Player name='Player 2' symbol='O'></Player>
+        </ol>
+        Game Board
+      </div>
+    </main>
+  )
+}
+
+export default App
